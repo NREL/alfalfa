cfc815515da50bb773406a3eb00703d088b02159
Kyle Benne <kyle.benne@nrel.gov>
2017-08-16 15:22:51 -0500

    Add primitive test file for worker

---
+++ worker/test/test.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+rootpath = os.path.dirname(__file__)
+sys.path.append(os.path.join(rootpath,'../'))
+from parsevariables import Variables
+
+mapping_json_path = os.path.join(rootpath,'workflow/reports/haystack_report_mapping.json')
+variables_xml_path = os.path.join(rootpath,'workflow/reports/export_bcvtb_report_variables.cfg')
+
+iovars = Variables(variables_xml_path, mapping_json_path)
+
+print(iovars.outputIndex("7fd5d8f3-1993-4f81-9b5a-e73921d63c08")) # return 6
+print(iovars.outputIndex("wrong")) # return -1
