fba65929b058c034704f5e19387b111699ff7ecb
goldcoder01 <yanfei.li@nrel.gov>
2019-04-22 17:23:13 -0600

    working on start() API

--- client/boptest.py
+++ client/boptest.py
@@ -38,114 +38,46 @@ class Boptest:
         # This is done not via the haystack api, but through a graphql api
         mutation = 'mutation { addSite(osmName: "%s", uploadID: "%s") }' % (filename, uid)
         response = requests.post(self.url + '/graphql', json={'query': mutation})
+        self.uuid = uid
+        self.modelname = filename
+
+        return (self.uuid, self.modelname)
 
     # Start a simulation for model identified by id. The id should corrsespond to 
     # a return value from the submit method
     # sim_params should be parameters such as start_time, end_time, timescale,
     # and others. The details need to be further defined and documented
     def start(self,  **sim_params):
-        site_ref = 'id'
-        real_time_flag = False
-        time_scale = 5
-        user_start_Datetime = sim_params['start_datetime']
-        user_end_Datetime = sim_params['end_datetime']
-        sim_path = 'simulate/'
-        if not os.path.exists(sim_path):
-            os.makedirs(sim_path)
-            
-
-        fmupath = os.path.join(os.getcwd())+'/'+sim_path+'model.fmu'
-        if not os.path.isfile(fmupath):
-            copyfile("sharedfiles/wrapped.fmu", fmupath)
-
-        #tagpath = os.path.join(os.getcwd())+'/'+sim_path+'tags.json'
-        #if not os.path.isfile(tagpath):
-        #    copyfile("sharedfiles/tags.json", tagpath)
-
-        print ("hey fmupath: ", fmupath)
-        mongo_client = MongoClient('mongodb://mongo:27017/')
-        mongodb = mongo_client['admin']
-        recs = mongodb.recs
-        recs.update_one({"_id": site_ref}, {"$set": {"rec.simStatus": "s:Running"}}, False)
-
-        config = { 'fmupath': fmupath, 'step': 60 }
         
-                
-        tc = testcase.TestCase(config)
-        u = {}
-        #run the FMU simulation
-        kstep=0
-        stop = False
-        simtime = 0
-        while simtime < 120 and not stop:
-            y_output = tc.advance(u)
-            simtime = tc.final_time
-            output_time_string = 's:%s' %(simtime)
-            recs.update_one( {"_id": site_ref}, { "$set": {"rec.datetime": output_time_string, "rec.simStatus":"s:Running"} } )
+        time_scale     = sim_params["time_scale"]
+        start_datetime = sim_params["start_datetime"]
+        end_datetime   = sim_params["end_datetime"]
+        realtime       = sim_params["realtime"]
 
-        return 0
-        
-    
-    def get_tag_data(self, tag_filepath):
-        '''
-        Purpose: retrieving the haystack tagged data, 
-              which are model-exchange variables in FMU. 
-        Inputs:  a json file containing all the tagged data.
-        Returns: a list for all the tagged data, with the tagging properties
-              like id, dis, site_ref, etc...
-        '''
-        with open( tag_filepath ) as json_data:
-            tag_data = json.load(json_data)
-     
-        return tag_data
-
-
-    #This is the function to create tags
-    def create_DisToID_dictionary(self, tag_filepath):
-        '''
-        Purpose: matching the haystack display-name and IDs
-        Inputs:  a json file containing all the tagged data
-        Returns: a dictionary mathching all display-names and IDs
-             a dictionary matching all inputs and IDs
-             a dictionary matching all outputs and IDs 
-             a dictionary matching all IDs and display-names 
-        '''
-        dis_and_ID={}
-        inputs_and_ID = {}
-        outputs_and_ID = {}
-        id_and_dis={}
-
-        tag_data = get_tag_data(tag_filepath)
-
-        for point in tag_data:
-            var_name = point['dis'].replace('s:','')
-            var_id = point['id'].replace('r:','')
-
-            dis_and_ID[var_name] = var_id
-            id_and_dis[var_id] = var_name
-
-            if 'writable' in point.keys():
-                #it means it is a input variable.
-                #clean the var-name, discarding: ':input','s:','r:'
-                #input_var = var_name.replace(':input','')
-                #input_var = input_var.replace('s:','')
-                inputs_and_ID[var_name] = var_id
-
-            if 'writable' not in point.keys() and 'point' in point.keys():
-                #it means it is an output variable, plus not sitetag.
-                #clean the var-name, discarding: ':output','s:','r:'
-                #output_var = var_name.replace(':output','')
-                #output_var = output_var.replace('s:','')
-                outputs_and_ID[var_name] = var_id
-
-        return (dis_and_ID, inputs_and_ID, outputs_and_ID, id_and_dis)
-
+        mutation = 'mutation { runSite(siteRef: "%s", startDatetime: "%s", endDatetime: "%s", timescale: "%s", realtime: "%s") }' % (self.uuid, start_datetime, end_datetime, time_scale, realtime)
+          
+        mutation = mutation.replace('timescale: "5"', 'timescale: 5')
+ 
+        payload = {'query': mutation}
+         
+        response = requests.post(self.url + '/graphql', json=payload ) 
+        print('starting simu API response: \n')
+        print(response.text)
+      
 
 
     # Stop a simulation for model identified by id
-    def stop(self, id):
-        pass
+    def stop(self, id):    
+        mutation = 'mutation { stopSite(siteRef: "%s") }' % (id)
 
+        payload = {'query': mutation}
+        
+        response = requests.post(self.url + '/graphql', json=payload )
+        print('stopping simu API response: \n')
+        print(response.text)
+
+ 
+        
     # Return the input values for simulation identified by id,
     # in the form of a dictionary. See setInputs method for dictionary format
     def inputs(self, id):
