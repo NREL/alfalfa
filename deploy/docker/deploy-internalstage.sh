98f81e5ec2de425d3505d3a5713b01b466eaed13
Petersen, Anya <Anya.Petersen@nrel.gov>
2019-04-24 21:27:38 -0600

    deploy scripts do work.

--- deploy/docker/deploy-internalstage.sh
+++ deploy/docker/deploy-internalstage.sh
@@ -7,6 +7,12 @@
 #build images
 export BT_DOCKER_REGISTRY_URI=ic-docker-registry.nrel.gov:5000/
 export BT_ENV=stage
+export STAGE_DOMAIN=ic-docker-registry.nrel.gov #1lv11openstu01.nrel.gov 1lv11inten03.nrel.gov
+# export WEB_REGISTRY_URI=313781303390.dkr.ecr.us-east-1.amazonaws.com/queue/web
+# export WORKER_REGISTRY_URI=313781303390.dkr.ecr.us-east-1.amazonaws.com/queue/worker
+# export AWS_ACCESS_KEY_ID=user
+# export AWS_SECRET_ACCESS_KEY=password
+# export NODE_ENV=none
 docker-compose -f ./deploy/docker/docker-compose-noaws.yml build
 
 #push images to registry
@@ -14,5 +20,6 @@ docker push "${BT_DOCKER_REGISTRY_URI}bt-web-${BT_ENV}:latest"
 docker push "${BT_DOCKER_REGISTRY_URI}bt-worker-${BT_ENV}:latest"
 
 #copy stack definition to staging server
-scp ./deploy/docker/docker-compose-development.yml 1lv11openstu01.nrel.gov:docker-compose.yml
-ssh 1lv11openstu01.nrel.gov "docker pull ${BT_DOCKER_REGISTRY}bt-web-${BT_ENV}:latest && docker pull ${BT_DOCKER_REGISTRY}bt-worker-${BT_ENV}:latest  && docker stack rm bt-${BT_ENV} && sleep 15 && docker stack deploy -c docker-compose.yml bt-${BT_ENV}"
+scp ./deploy/docker/docker-compose-noaws.yml $STAGE_DOMAIN:docker-compose.yml
+scp ./deploy/docker/.env-internalstage $STAGE_DOMAIN:.env
+ssh $STAGE_DOMAIN "export $(cat .env) && docker pull ${BT_DOCKER_REGISTRY_URI}bt-web-${BT_ENV}:latest && docker pull ${BT_DOCKER_REGISTRY_URI}bt-worker-${BT_ENV}:latest  && docker stack rm bt-${BT_ENV} && sleep 20 && docker stack deploy -c docker-compose.yml bt-${BT_ENV}"
--- deploy/docker/deploy-local.sh
+++ deploy/docker/deploy-local.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
  # requires that 'docker swarm init' has been run on development machine
+
+export $(cat deploy/docker/.env-internalstage)
+export BT_DOCKER_REGISTRY_URI=
 export BT_ENV=development
-export WEB_REGISTRY_URI=313781303390.dkr.ecr.us-east-1.amazonaws.com/queue/web
-export WORKER_REGISTRY_URI=313781303390.dkr.ecr.us-east-1.amazonaws.com/queue/worker
-export AWS_ACCESS_KEY_ID=user
-export AWS_SECRET_ACCESS_KEY=password
-export NODE_ENV=none
-export S3_URL=http://minio:9000
+
 docker-compose -f ./deploy/docker/docker-compose-noaws.yml build
 docker stack rm "bt-${BT_ENV}"
 sleep 25s #docker swarm sometimes has errors w/ default network container if you try to redeploy immediately
