29b117f10c69702bf740d217683dad4d8333fca0
goldcoder01 <yanfei.li@nrel.gov>
2019-04-25 00:54:32 -0600

    Basically APIs are done

--- client/boptest.py
+++ client/boptest.py
@@ -19,13 +19,16 @@ class Boptest:
     def submit(self, path):
         filename = os.path.basename(path)
         uid = str(uuid.uuid1())
+        
         key = 'uploads/' + uid + '/' + filename
         payload = {'name': key}
 
         # Get a template for the file upload form data
         # The server has an api to give this to us
         response = requests.post(self.url + '/upload-url', json=payload)
+        
         json = response.json()
+        #print ('initial response: ', json)
         postURL = json['postURL']
         formData = json['formData']
         formData['file'] = open(path, 'rb')
@@ -40,8 +43,17 @@ class Boptest:
         response = requests.post(self.url + '/graphql', json={'query': mutation})
         self.uuid = uid
         self.modelname = filename
-
-        return (self.uuid, self.modelname)
+        #print('second response: ', response.text)
+        #payload = {'name': siteRef} 
+        response = requests.get(self.url+'/api/nav')
+        #print('third response: ', response.text )   
+        r = response.text
+        r = r.splitlines()
+        line_seek = r[-1].split(',')
+        siteref = line_seek[-1].replace('@','')
+        #print (siteref)
+        self.siteref = siteref
+        return (self.siteref, self.modelname)
 
     # Start a simulation for model identified by id. The id should corrsespond to 
     # a return value from the submit method
@@ -53,16 +65,19 @@ class Boptest:
         start_datetime = sim_params["start_datetime"]
         end_datetime   = sim_params["end_datetime"]
         realtime       = sim_params["realtime"]
-
-        mutation = 'mutation { runSite(siteRef: "%s", startDatetime: "%s", endDatetime: "%s", timescale: "%s", realtime: "%s") }' % (self.uuid, start_datetime, end_datetime, time_scale, realtime)
+        
+        mutation = 'mutation {\n' + ' runSite(siteRef: "%s",\n startDatetime: "%s",\n endDatetime: "%s",\n timescale: "%s",\n realtime: "%s") \n}' % (self.siteref, start_datetime, end_datetime, time_scale, realtime)
           
         mutation = mutation.replace('timescale: "5"', 'timescale: 5')
- 
+        #print (mutation)
+        
         payload = {'query': mutation}
          
-        response = requests.post(self.url + '/graphql', json=payload ) 
+        response = requests.post(self.url + '/graphql', data=payload ) 
         print('starting simu API response: \n')
         print(response.text)
+        print (response.status_code)
+
       
 
 
@@ -76,12 +91,49 @@ class Boptest:
         print('stopping simu API response: \n')
         print(response.text)
 
+
+    # remove a site for model identified by id
+    def remove(self, id):
+        mutation = 'mutation { removeSite(siteRef: "%s") }' % (id)
+
+        payload = {'query': mutation}
+
+        response = requests.post(self.url + '/graphql', json=payload )
+        print('remove site API response: \n')
+        print(response.text) 
+       
  
-        
     # Return the input values for simulation identified by id,
     # in the form of a dictionary. See setInputs method for dictionary format
-    def inputs(self, id):
-        pass
+    def inputs(self):
+          
+        viewer = '{viewer {\n'+ ' sites(\n ' + ('  siteRef: "%s"') %(self.siteref) +'){\n ' + ' points {\n' + '  dis\n' + '  tags{\n' + '   key' +' '+ 'value' + '\n}}}}}'
+
+        payload = {'query': viewer}
+        response = requests.post(self.url+'/graphql', json=payload)
+        response = response.json()
+        response = response["data"]
+        response = response["viewer"]
+        response = response["sites"]
+        response = response[0]
+        response = response["points"]
+        input_map={}
+        for x in response:
+            var_name = x['dis']
+            tags = x['tags']
+           
+            for y in tags: 
+                if y['key']=="id":
+                    var_id = y['value']
+                if y['key']=="curVal":
+                    print ( y['value'] )
+                #if y['key']=="writable":
+                #    input_map[var_id] = var_name
+            input_map[var_id] = var_name
+        self.inputs = input_map
+        print('hey input-map: ',input_map)
+        return self.inputs
+
     # Set inputs for model identified by id
     # The inputs argument should be a dictionary of 
     # with the form
@@ -89,13 +141,66 @@ class Boptest:
     #  input_name: { level_1: value_1, level_2: value_2...},
     #  input_name2: { level_1: value_1, level_2: value_2...}
     # }
-    def setInputs(self, id, **inputs):
-        pass
+    def setInputs(self, id, **user_inputs): 
+        url    = "http://localhost:80/api/pointWrite"
+        header = { "Accept":"application/json", "Content-Type":"application/json; charset=utf-8" }
+        for var in user_inputs.keys():
+            value_inputs = user_inputs[var]
+            for level in value_inputs.keys():
+                the_value = value_inputs[level]
+                data = json.dumps(
+                       {
+                          "meta": {"ver":"2.0"},
+                          "rows": [ { "id" :"r:" + id, \
+                          "level": "n:"+str(level), \
+                          "who" : "s:" , \
+                          "val" : "n:"+str(the_value),\
+                          "duration": "s:"
+                                    }
+                                  ],
+                          "cols": [ {"name":"id"}, {"name":"level"}, {"name":"val"}, {"name":"who"}, {"name":"duration"} ]
+                       }
+                       )
+                writing_response = requests.post(url=url, headers=header, data=data)
+        if writing_response.status_code==200:
+            print("Congratulations! Your inputs were applied!")
+            print (writing_response.text)                
+        else:
+            print("Poor Boy! You inputs have some issue!")        
+
+
     # Return a dictionary of the output values
     # result = {
     # output_name1 : output_value1,
     # output_name2 : output_value2
     #}
     def outputs(self, id):
-        pass
+        
+        url    = "http://localhost:80/api/read"
+        header = { "Accept":"application/json", "Content-Type":"text/zinc" }
+        header2 = { "Accept":"application/json", "Content-Type":"application/json; charset=utf-8" }
+        payload = 'ver:"2.0"\n' + 'filter,limit\n' + ('id==@"%s"')%(id) + '\,1000'
+        data = json.dumps(
+                       {
+                          "meta": {"ver":"2.0"},
+                          "rows": [ { "id" :"r:" + id, \
+                          "who" : "s:" , \
+                          "val" : "n:" , \
+                          "duration": "s:"
+                                    }
+                                  ],
+                          "cols": [ {"name":"id"}, {"name":"val"}, {"name":"who"}, {"name":"duration"} ]
+                       }
+                       )
+ 
+        reading_response = requests.post(url=url, headers=header2, data=data)
+
+        
+        if reading_response.status_code==200:
+            print("Congratulations! Outputs were retrieved!")
+            print (reading_response.text)
+        else:
+            print("Poor boy, outputs have issues!")
+     
+
 
--- test/test.py
+++ test/test.py
@@ -4,10 +4,50 @@
 #from os.path import dirname
 import sys
 import os.path
+import datetime
+import time
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../client/')
 import boptest
 
 bop = boptest.Boptest()
-bop.submit('wrapped.fmu')
+
+(siteref, modelname) = bop.submit('wrapped.fmu')
+
+time.sleep(3)
+
+time_scale=5
+start_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+tmp   = datetime.datetime.now() + datetime.timedelta(minutes=10)
+end_datetime   = tmp.strftime("%Y-%m-%d %H:%M:%S")
+realtime = False
+input_params = { "time_scale"    :  time_scale, 
+                 "start_datetime":  start_datetime,
+                 "end_datetime"  :  end_datetime,
+                 "realtime"     :  realtime
+               }
+bop.start(**input_params)
+
+#bop.remove(siteref)
+
+time.sleep(5)
+model_inputs = bop.inputs()
+
+yanfei_inputs = {"oveAct_u": { '1': 0.625 } }
+inputs = yanfei_inputs.keys()
+
+for x in model_inputs.keys():
+    for y in inputs:
+        #y = "u'"+y
+        print (y)
+        print(model_inputs[x])
+        if model_inputs[x] == y:
+            print('Hey there!')
+            input_id = x
+            bop.setInputs(input_id, **yanfei_inputs)
+
+for x in model_inputs.keys():
+    if 'TRooAir_y' in model_inputs[x]:
+        id = x
+        bop.outputs(id)
 
