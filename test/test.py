9d01e10e1231ebaad33ea12c24d6a360eafac082
Kyle Benne <kyle.benne@nrel.gov>
2020-04-01 13:23:52 -0500

    Add example of integration test

---
+++ test/test.py
@@ -0,0 +1,15 @@
+import sys
+from alfalfaclient import AlfalfaClient
+
+client = AlfalfaClient(url='http://web')
+result = client.submit('/test/SmallOffice_Unitary_1.osm')
+
+# Replace with test framework
+if result:
+  print("success")
+  sys.exit(0);
+else:
+  print("failure")
+  sys.exit(1);
+
+
