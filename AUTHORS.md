38b8f2038fbc05dcf630b7d4c1837bb1a17e49ab
Nicholas Long <nicholas.long@nrel.gov>
2020-04-01 12:29:56 -0600

    add unit tests. note that this breaks alfalfa probably since init has changed

---
+++ AUTHORS.md
@@ -0,0 +1,9 @@
+# Contributors
+
+* Kyle Benne <kbenne>
+* Cory Mosiman <corymosiman12>
+* Austin Viveiros <aviveros11>
+* Nicholas Long <nllong>
+* Brian Ball <brianlball>
+* Willy Bernal <willybernal>
+* Yanfei Li <goldcoder01>
--- worker/step_sim/__init__.py
+++ worker/step_sim/__init__.py
@@ -1,5 +1,5 @@
-from .model_advancer import ModelAdvancer  # noqa
-from .model_logger import ModelLogger  # noqa
-from .fmu_model_advancer import FMUModelAdvancer  # noqa
-from .osm_model_advancer import OSMModelAdvancer  # noqa
-from .step_sim_utils import *  # noqa
+# from .model_advancer import ModelAdvancer  # noqa
+# from .model_logger import ModelLogger  # noqa
+# from .fmu_model_advancer import FMUModelAdvancer  # noqa
+# from .osm_model_advancer import OSMModelAdvancer  # noqa
+# from .step_sim_utils import *  # noqa
