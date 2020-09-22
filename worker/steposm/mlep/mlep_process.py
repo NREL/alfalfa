d540779c46ca2d4fa7f67f1d7d44bc3ec538cd35
Kyle Benne <kyle.benne@nrel.gov>
2020-09-22 00:02:06 -0500

    Bump mlep buffer size

--- worker/steposm/mlep/mlep_process.py
+++ worker/steposm/mlep/mlep_process.py
@@ -199,7 +199,7 @@ class MlepProcess:
     def read(self):
         # Read Packet
         if self.is_running:
-            packet = self.comm_socket.recv(5000)
+            packet = self.comm_socket.recv(50000)
         else:
             packet = ''
             print('Co-simulation is not running.')
