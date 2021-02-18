27c7cc355c16aef100ffd6b32ba2e471d5b21abc
Anya Petersen <anyaelena@gmail.com>
2021-02-17 20:02:50 -0700

    image names.

--- .github/workflows/publish_to_docker.sh
+++ .github/workflows/publish_to_docker.sh
@@ -1,6 +1,6 @@
 export VERSION_TAG=develop
-export WEB_REGISTRY_URI=NREL/alfalfa-web
-export WORKER_REGISTRY_URI=NREL/alfalfa-worker
+export WEB_REGISTRY_URI=nrel/alfalfa-web
+export WORKER_REGISTRY_URI=nrel/alfalfa-worker
 docker-compose build
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker push ${WEB_REGISTRY_URI}:${VERSION_TAG}
