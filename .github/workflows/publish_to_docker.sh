3359c4bcf4f5dc956ce8f3bdee4e23dc6cb49567
Anya Petersen <anyaelena@gmail.com>
2021-02-17 21:28:14 -0700

    also publish latest if in master branch.

--- .github/workflows/publish_to_docker.sh
+++ .github/workflows/publish_to_docker.sh
@@ -3,13 +3,22 @@ export WORKER_REGISTRY_URI=nrel/alfalfa-worker
 
 if [ "${GITHUB_REF}" == "refs/heads/develop" ]; then
     VERSION_TAG="develop"
-#TODO remove docker-pub check
 elif [ "${GITHUB_REF}" == "refs/heads/docker-pub" ]; then
     VERSION_TAG="$(python ./.github/workflows/parse_version.py)"
 elif [ "${GITHUB_REF}" == "refs/heads/master" ]; then
     VERSION_TAG="$(python ./.github/workflows/parse_version.py)"
+fi
 
 docker-compose build
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-docker push ${WEB_REGISTRY_URI}:${VERSION_TAG}
-docker push ${WORKER_REGISTRY_URI}:${VERSION_TAG}
+docker push ${WEB_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+docker push ${WORKER_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+
+if [ "${GITHUB_REF}" == "refs/heads/master" ]; then
+    # Deploy master as the latest.
+    docker tag ${WEB_REGISTRY_URI}:${VERSION_TAG} ${WEB_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
+    docker tag ${WORKER_REGISTRY_URI}:${VERSION_TAG} ${WORKER_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
+
+    docker push ${WEB_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
+    docker push ${WORKER_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
+fi
