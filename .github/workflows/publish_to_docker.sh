c61ce4b67a7d438b24d8d1e34e8a95bef7a96b84
Anya Petersen <anyaelena@gmail.com>
2021-02-17 21:13:49 -0700

    Extract version from Poetry file

--- .github/workflows/publish_to_docker.sh
+++ .github/workflows/publish_to_docker.sh
@@ -1,6 +1,14 @@
-export VERSION_TAG=develop
 export WEB_REGISTRY_URI=nrel/alfalfa-web
 export WORKER_REGISTRY_URI=nrel/alfalfa-worker
+
+if [ "${GITHUB_REF}" == "refs/heads/develop" ]; then
+    VERSION_TAG="develop"
+#TODO remove docker-pub check
+elif [ "${GITHUB_REF}" == "refs/heads/docker-pub" ]; then
+    VERSION_TAG="$(python ./.github/workflows/parse_version.py)"
+elif [ "${GITHUB_REF}" == "refs/heads/master" ]; then
+    VERSION_TAG="$(python ./.github/workflows/parse_version.py)"
+
 docker-compose build
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker push ${WEB_REGISTRY_URI}:${VERSION_TAG}
