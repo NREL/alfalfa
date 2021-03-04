cacfcb45286729c7107b0491de4f358f8e564087
Anya Petersen <anyaelena@gmail.com>
2021-03-04 11:49:40 -0700

    some debugging of "invalid reference format"

--- .github/workflows/publish_to_docker.sh
+++ .github/workflows/publish_to_docker.sh
@@ -1,6 +1,7 @@
 export WEB_REGISTRY_URI=nrel/alfalfa-web
 export WORKER_REGISTRY_URI=nrel/alfalfa-worker
 
+# Here VERSION_TAG will override what is loaded from .env
 if [ "${GITHUB_REF}" == "refs/heads/develop" ]; then
     export VERSION_TAG="develop"
 elif [ "${GITHUB_REF}" == "refs/heads/publish-grafana" ]; then
@@ -11,8 +12,11 @@ fi
 
 docker-compose -f docker-compose.yml -f docker-compose-historian.yml build
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+echo "pushing ${WEB_REGISTRY_URI}:${VERSION_TAG}"
 docker push ${WEB_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+echo "pushing ${WORKER_REGISTRY_URI}:${VERSION_TAG}"
 docker push ${WORKER_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+echo "pushing ${HISTORIAN_GUI_REGISTRY_URI}:${VERSION_TAG}"
 docker push ${HISTORIAN_GUI_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
 
 if [ "${GITHUB_REF}" == "refs/heads/master" ]; then
