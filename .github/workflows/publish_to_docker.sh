8fa0765904a61a92f61e95165512f6c80b8b1030
Anya Petersen <anyaelena@gmail.com>
2021-03-03 21:18:01 -0700

    test publishing of grafana image.

--- .github/workflows/publish_to_docker.sh
+++ .github/workflows/publish_to_docker.sh
@@ -3,22 +3,27 @@ export WORKER_REGISTRY_URI=nrel/alfalfa-worker
 
 if [ "${GITHUB_REF}" == "refs/heads/develop" ]; then
     export VERSION_TAG="develop"
+elif [ "${GITHUB_REF}" == "refs/heads/publish-grafana" ]; then
+    export VERSION_TAG="experimental"
 elif [ "${GITHUB_REF}" == "refs/heads/master" ]; then
     export VERSION_TAG="$(python ./.github/workflows/parse_version.py)"
 fi
 
-docker-compose build
+docker-compose -f docker-compose.yml -f docker-compose-historian.yml
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker push ${WEB_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
 docker push ${WORKER_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
+docker push ${HISTORIAN_GUI_REGISTRY_URI}:${VERSION_TAG}; (( exit_status = exit_status || $? ))
 
 if [ "${GITHUB_REF}" == "refs/heads/master" ]; then
     # Deploy master as the latest.
     docker tag ${WEB_REGISTRY_URI}:${VERSION_TAG} ${WEB_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
     docker tag ${WORKER_REGISTRY_URI}:${VERSION_TAG} ${WORKER_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
+    docker tag ${HISTORIAN_GUI_REGISTRY_URI}:${VERSION_TAG} ${HISTORIAN_GUI_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
 
     docker push ${WEB_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
     docker push ${WORKER_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
+    docker push ${HISTORIAN_GUI_REGISTRY_URI}:latest; (( exit_status = exit_status || $? ))
 fi
 
 exit $exit_status
