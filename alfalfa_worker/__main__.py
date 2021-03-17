6daf86e48e23f86a3478e69cfa7cb33f9d1e3611
Anya Petersen <anyaelena@gmail.com>
2021-03-16 22:36:58 -0600

    Print the error and traceback if worker dies during startup.

--- alfalfa_worker/__main__.py
+++ alfalfa_worker/__main__.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
     try:
         worker = Worker()
         worker.worker_logger.logger.info("Worker initialized")
-    except BaseException:  # TODO: what exceptions to catch?
-        print('Exception while starting up alfalfa_worker', file=sys.stderr)
+    except BaseException as e:  # TODO: what exceptions to catch?
+        tb = traceback.format_exc()
+        print("Exception while starting up alfalfa_worker error {} with {}".format(e, tb), file=sys.stderr)
         sys.exit(1)
 
     try:
