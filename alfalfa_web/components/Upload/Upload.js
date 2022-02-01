a1c046f53af3d7988f11acf4d05c06db62da2754
Nicholas Long <nicholas.long@nrel.gov>
2022-01-31 18:49:32 -0700

    prettier formatting of javascript

--- alfalfa_web/components/Upload/Upload.js
+++ alfalfa_web/components/Upload/Upload.js
@@ -1,48 +1,47 @@
 /***********************************************************************************************************************
-*  Copyright (c) 2008-2022, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-*  following conditions are met:
-*
-*  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-*  disclaimer.
-*
-*  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
-*  disclaimer in the documentation and/or other materials provided with the distribution.
-*
-*  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote products
-*  derived from this software without specific prior written permission from the respective party.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-*  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE UNITED STATES GOVERNMENT, OR THE UNITED
-*  STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-*  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-*  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-*  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-***********************************************************************************************************************/
+ *  Copyright (c) 2008-2022, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ *  following conditions are met:
+ *
+ *  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ *  disclaimer.
+ *
+ *  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ *  disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ *  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote products
+ *  derived from this software without specific prior written permission from the respective party.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE UNITED STATES GOVERNMENT, OR THE UNITED
+ *  STATES DEPARTMENT OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***********************************************************************************************************************/
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import IconButton from '@material-ui/core/IconButton';
-import {FileUpload} from '@material-ui/icons';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
-import { InputLabel, InputLabelProps } from '@material-ui/core/Input';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import 'normalize.css/normalize.css';
-import styles from './Upload.scss';
-import {cyan500, red500, greenA200} from '@material-ui/core/colors';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import { v1 as uuidv1 } from 'uuid';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import IconButton from "@material-ui/core/IconButton";
+import { FileUpload } from "@material-ui/icons";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import { InputLabel, InputLabelProps } from "@material-ui/core/Input";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import "normalize.css/normalize.css";
+import styles from "./Upload.scss";
+import { cyan500, red500, greenA200 } from "@material-ui/core/colors";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { v1 as uuidv1 } from "uuid";
+import { withStyles } from "@material-ui/core/styles";
 
 class FileInput extends React.Component {
-
   constructor(props) {
     super(props);
     this.fileInputRef = React.createRef();
@@ -53,17 +52,17 @@ class FileInput extends React.Component {
       filename: "",
       file: ""
     };
-  };
+  }
 
   static propTypes = {
     hint: PropTypes.string,
-    onFileChange: PropTypes.func,
+    onFileChange: PropTypes.func
   };
 
   handleFileChange = (evt) => {
     const file = evt.target.files[0];
     this.props.onFileChange(file);
-    this.setState({filename: file.name, file: file});
+    this.setState({ filename: file.name, file: file });
   };
 
   handleTextInputClick = (evt) => {
@@ -71,7 +70,7 @@ class FileInput extends React.Component {
     if (el.addEventListener) {
       el.addEventListener("change", this.handleFileChange, false);
     } else {
-      el.attachEvent('onchange', this.handleFileChange);
+      el.attachEvent("onchange", this.handleFileChange);
     }
     el.click();
   };
@@ -80,19 +79,20 @@ class FileInput extends React.Component {
     return (
       <div>
         <input className={styles.hidden} type="file" ref={this.fileInputRef} onInput={this.handleFileChange} />
-        <TextField fullWidth={true} label='Select Model' onClick={this.handleTextInputClick} value={this.state.filename}
+        <TextField
+          fullWidth={true}
+          label="Select Model"
+          onClick={this.handleTextInputClick}
+          value={this.state.filename}
           InputLabelProps={{
             shrink: this.state.filename != ""
-          }}
-        >
-        </TextField>
+          }}></TextField>
       </div>
-    )
-  }
+    );
+  };
 }
 
 class Upload extends React.Component {
-
   constructor() {
     super();
     this.onModelFileChange = this.onModelFileChange.bind(this);
@@ -105,7 +105,7 @@ class Upload extends React.Component {
       uploadID: null,
       completed: 0
     };
-  };
+  }
 
   static propTypes = {
     //className: PropTypes.string,
@@ -117,15 +117,15 @@ class Upload extends React.Component {
   };
 
   onModelFileChange(file) {
-    this.setState({modelFile: file, completed: 0, uploadID: uuidv1()});
+    this.setState({ modelFile: file, completed: 0, uploadID: uuidv1() });
   }
 
   onWeatherFileChange(file) {
-    this.setState({weatherFile: file, completed: 0});
+    this.setState({ weatherFile: file, completed: 0 });
   }
 
   onClick(onCompleteProp) {
-    if( this.state.modelFile ) {
+    if (this.state.modelFile) {
       const key = `uploads/${this.state.uploadID}/${this.state.modelFile.name}`;
 
       const request = new XMLHttpRequest();
@@ -144,23 +144,22 @@ class Upload extends React.Component {
 
       const uploadProgress = (evt) => {
         if (evt.lengthComputable) {
-          var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+          var percentComplete = Math.round((evt.loaded * 100) / evt.total);
           if (percentComplete > 100) {
-            this.setState({completed: 100});
+            this.setState({ completed: 100 });
           } else {
-            this.setState({completed: percentComplete});
+            this.setState({ completed: percentComplete });
           }
-        }
-        else {
-          console.log('percent: unable to compute');
+        } else {
+          console.log("percent: unable to compute");
         }
       };
 
       const uploadFile = () => {
         const response = JSON.parse(request.responseText);
 
-        if ( ! response ) {
-          console.log('Failed to acquire upload url');
+        if (!response) {
+          console.log("Failed to acquire upload url");
           return;
         }
 
@@ -172,31 +171,31 @@ class Upload extends React.Component {
         xhr.addEventListener("abort", uploadCanceled, false);
 
         // TODO: Need to configure this on server side
-        xhr.open('POST', response.url, true);
+        xhr.open("POST", response.url, true);
 
         let formData = new FormData();
-        console.log(response)
+        console.log(response);
         Object.entries(response.fields).forEach(([key, value]) => {
-            formData.append(key, value);
+          formData.append(key, value);
         });
-        formData.append('file', this.state.modelFile);
+        formData.append("file", this.state.modelFile);
 
-        xhr.send(formData);  // multipart/form-data
+        xhr.send(formData); // multipart/form-data
       };
 
-      const url = '/upload-url';
+      const url = "/upload-url";
       const params = JSON.stringify({ name: key });
-      request.open('POST', url, true);
+      request.open("POST", url, true);
       request.setRequestHeader("Content-type", "application/json; charset=utf-8");
       request.addEventListener("load", uploadFile, false);
       request.send(params);
     } else {
-      console.log('Select file to upload');
+      console.log("Select file to upload");
     }
   }
 
   modelFileHint() {
-    if( this.state.modelFile ) {
+    if (this.state.modelFile) {
       return this.state.modelFile.name;
     } else {
       return undefined;
@@ -204,10 +203,10 @@ class Upload extends React.Component {
   }
 
   weatherFileHint() {
-    if( this.state.weatherFile ) {
+    if (this.state.weatherFile) {
       return this.state.weatherFile.name;
     } else {
-      return 'Select Weather File';
+      return "Select Weather File";
     }
   }
 
@@ -220,10 +219,18 @@ class Upload extends React.Component {
         <div className={styles.center}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-              <FileInput hint={this.modelFileHint()} onFileChange={this.onModelFileChange}/>
+              <FileInput hint={this.modelFileHint()} onFileChange={this.onModelFileChange} />
             </Grid>
             <Grid item xs>
-              <Button fullWidth={true} variant="contained" color="primary" onClick={() => {this.onClick(this.props.addJobProp)} }>Upload Model</Button>
+              <Button
+                fullWidth={true}
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  this.onClick(this.props.addJobProp);
+                }}>
+                Upload Model
+              </Button>
             </Grid>
           </Grid>
         </div>
@@ -232,10 +239,10 @@ class Upload extends React.Component {
   }
 }
 
-const localstyles = theme => ({
+const localstyles = (theme) => ({
   button: {
-    margin: theme.spacing(1),
-  },
+    margin: theme.spacing(1)
+  }
 });
 
 const withStyle = withStyles(localstyles)(Upload);
@@ -254,12 +261,12 @@ const runSimQL = gql`
 
 const withRunSim = graphql(runSimQL, {
   props: ({ mutate }) => ({
-    runSimProp: (uploadFilename, uploadID) => mutate({ variables: { uploadFilename, uploadID } }),
-  }),
+    runSimProp: (uploadFilename, uploadID) => mutate({ variables: { uploadFilename, uploadID } })
+  })
 })(withStyle);
 
 export default graphql(addJobQL, {
   props: ({ mutate }) => ({
-    addJobProp: (osmName, uploadID) => mutate({ variables: { osmName, uploadID } }),
-  }),
+    addJobProp: (osmName, uploadID) => mutate({ variables: { osmName, uploadID } })
+  })
 })(withRunSim);
--- alfalfa_web/server/dbops.js
+++ alfalfa_web/server/dbops.js
@@ -17,11 +17,11 @@ class WriteArray {
 function currentWinningValue(array) {
   for (var i = 0; i < array.val.length; ++i) {
     const v = array.val[i];
-    if (v || (v == 0)) {
+    if (v || v == 0) {
       return {
         val: v,
         level: i + 1
-      }
+      };
     }
   }
 
@@ -29,7 +29,7 @@ function currentWinningValue(array) {
 }
 
 function setOrNullArray(array, val, level, who) {
-  if (val || (val == 0) ) {
+  if (val || val == 0) {
     array.val[level - 1] = val;
     array.who[level - 1] = who;
   } else {
@@ -40,71 +40,101 @@ function setOrNullArray(array, val, level, who) {
 
 // Get a point by siteRef and display name
 function getPoint(siteRef, name, db) {
-  let mrecs = db.collection('recs');
-  return mrecs.findOne({site_ref: siteRef, "rec.dis": `s:${name}`});
+  let mrecs = db.collection("recs");
+  return mrecs.findOne({ site_ref: siteRef, "rec.dis": `s:${name}` });
 }
 
 function writePoint(id, siteRef, level, val, who, dur, db) {
-  return new Promise( (resolve,reject) => {
-    let writearrays = db.collection('writearrays');
-    let mrecs = db.collection('recs');
+  return new Promise((resolve, reject) => {
+    let writearrays = db.collection("writearrays");
+    let mrecs = db.collection("recs");
 
-    if (! level) { level = 1; }
+    if (!level) {
+      level = 1;
+    }
 
-    writearrays.findOne({_id: id}).then((array) => {
-      if( array ) {
-        // In this case the array already exists because it has been written to before
-        setOrNullArray(array, val, level, who);
-        writearrays.updateOne(
-          { "_id": array._id },
-          { $set: { "val": array.val, "who": array.who } }
-        ).then( () => {
-          const current = currentWinningValue(array);
-          if( current ) {
-            return mrecs.updateOne(
-              { "_id": array._id },
-              { $set: { "rec.writeStatus": "s:ok", "rec.writeVal": `s:${current.val}`, "rec.writeLevel": `n:${current.level}` }, $unset: { writeErr: "" } }
-            )
-          } else {
-            return mrecs.updateOne(
-              { "_id": array._id },
-              { $set: { "rec.writeStatus": "s:disabled" }, $unset: { "rec.writeVal": "", "rec.writeLevel": "", "rec.writeErr": ""} }
-            )
-          }
-        }).then( () => {
-          resolve(array);
-        }).catch( (err) => {
-          reject(err)
-        });
-      } else {
-        // In this case the point has never been written to and there is no
-        // existing write array in the db so we create a new one
-        let array = new WriteArray();
-        array._id = id;
-        array.siteRef = siteRef;
-        setOrNullArray(array, val, level, who);
-        writearrays.insertOne(array).then( () => {
-          const current = currentWinningValue(array);
-          if( current ) {
-            return mrecs.updateOne(
-              { "_id": array._id },
-              { $set: { "rec.writeStatus": "s:ok", "rec.writeVal": `s:${current.val}`, "rec.writeLevel": `n:${current.level}` }, $unset: { writeErr: "" } }
-            )
-          } else {
-            return mrecs.updateOne(
-              { "_id": array._id },
-              { $set: { "rec.writeStatus": "s:disabled" }, $unset: { "rec.writeVal": "", "rec.writeLevel": "", "rec.writeErr": ""} }
-            )
-          }
-        }).then(() => {
-          resolve(array);
-        }).catch((err) => {
-          reject(err);
-        });
-      }
-    }).catch((err) => {
-      reject(err);
-    });
+    writearrays
+      .findOne({ _id: id })
+      .then((array) => {
+        if (array) {
+          // In this case the array already exists because it has been written to before
+          setOrNullArray(array, val, level, who);
+          writearrays
+            .updateOne({ _id: array._id }, { $set: { val: array.val, who: array.who } })
+            .then(() => {
+              const current = currentWinningValue(array);
+              if (current) {
+                return mrecs.updateOne(
+                  { _id: array._id },
+                  {
+                    $set: {
+                      "rec.writeStatus": "s:ok",
+                      "rec.writeVal": `s:${current.val}`,
+                      "rec.writeLevel": `n:${current.level}`
+                    },
+                    $unset: { writeErr: "" }
+                  }
+                );
+              } else {
+                return mrecs.updateOne(
+                  { _id: array._id },
+                  {
+                    $set: { "rec.writeStatus": "s:disabled" },
+                    $unset: { "rec.writeVal": "", "rec.writeLevel": "", "rec.writeErr": "" }
+                  }
+                );
+              }
+            })
+            .then(() => {
+              resolve(array);
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        } else {
+          // In this case the point has never been written to and there is no
+          // existing write array in the db so we create a new one
+          let array = new WriteArray();
+          array._id = id;
+          array.siteRef = siteRef;
+          setOrNullArray(array, val, level, who);
+          writearrays
+            .insertOne(array)
+            .then(() => {
+              const current = currentWinningValue(array);
+              if (current) {
+                return mrecs.updateOne(
+                  { _id: array._id },
+                  {
+                    $set: {
+                      "rec.writeStatus": "s:ok",
+                      "rec.writeVal": `s:${current.val}`,
+                      "rec.writeLevel": `n:${current.level}`
+                    },
+                    $unset: { writeErr: "" }
+                  }
+                );
+              } else {
+                return mrecs.updateOne(
+                  { _id: array._id },
+                  {
+                    $set: { "rec.writeStatus": "s:disabled" },
+                    $unset: { "rec.writeVal": "", "rec.writeLevel": "", "rec.writeErr": "" }
+                  }
+                );
+              }
+            })
+            .then(() => {
+              resolve(array);
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 }
 
