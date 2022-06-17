cbe9f3133cf3424e6cecab857bb65e7f26c6cd19
TShapinsky <tobiasshapinsky@gmail.com>
2022-06-17 09:31:40 -0600

    fix calling an object

--- tests/jobs/modelica/conftest.py
+++ tests/jobs/modelica/conftest.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 def pytest_generate_tests(metafunc):
-    model_dir = Path(os.path.dirname(__file__)).parent().parent() / 'integration' / 'models'
+    model_dir = Path(os.path.dirname(__file__)).parent.parent / 'integration' / 'models'
     if "model_path" in metafunc.fixturenames:
         model_paths = [
             model_dir / 'simple_thermostat.fmu',
