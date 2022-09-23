4e924212d95afadafc00c66b879e9827b47d301e
Nicholas Long <1907354+nllong@users.noreply.github.com>
2022-09-22 18:54:57 -0600

    Update __init__.py

---
+++ alfalfa_worker/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = 'alfalfa-alfalfa_worker'
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+# TODO: remove these, I don't even think they exist as shown anymore!
+# Not sure that these are needed here. Leave for now...
+__all__ = ['add_site',
+           'lib',
+           'run_sim',
+           'step_sim']
