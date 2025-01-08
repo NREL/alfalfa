d5e9098908025e4d3bbee18e5e7671ab45cfd709
TShapinsky <tobiasshapinsky@gmail.com>
2025-01-08 15:42:14 -0700

    fix step duration

--- tests/worker/jobs/step_run_mock_job.py
+++ tests/worker/jobs/step_run_mock_job.py
@@ -15,6 +15,7 @@ class StepRunMockJob(MockJob, StepRunBase):
         StepRunBase.__init__(self, run_id, realtime, timescale, external_clock, start_datetime, end_datetime, skip_site_init=True, skip_stop_db_writes=True)
         self.options.warmup_is_first_step = True
         self.options.timestep_duration = timedelta(minutes=1)
+        self.simulation_step_duration = 1
         self.run.sim_time = self.options.start_datetime
 
     def get_sim_time(self) -> datetime.datetime:
@@ -25,9 +26,9 @@ class StepRunMockJob(MockJob, StepRunBase):
 
     @message
     def set_simulation_step_duration(self, simulation_step_duration):
-        self.options.timestep_duration = timedelta(minutes=simulation_step_duration)
+        self.simulation_step_duration = simulation_step_duration
 
     @message
     def advance(self):
-        sleep(self.options.timestep_duration)
+        sleep(self.simulation_step_duration)
         self.run.sim_time = self.run.sim_time + self.options.timestep_duration
